8e79352b2646ec0c6e5a867529d44dc373a8094f
Michael Ruzmetov <mimisha2014@gmail.com>
2023-11-26 06:30:58 +0100

    prompt refactor

--- ai_backend/llm_deploy.py
+++ ai_backend/llm_deploy.py
@@ -8,7 +8,7 @@ with open("api-key.txt", "r") as f:
 system_instructions = """
 - You will receive a series of data chunks, each labeled as "RFP Data Chunk #X", where X is the chunk's number.
 - Memorize the details in each chunk sequentially as they are provided.
-- Don't stop memorizing until a key word "STOPPY" appears!
+- Don't stop memorizing until I send a word "STOPPY" to you.
 - After that, ask for the next instructions.
 """
 response = client.chat.completions.create(
@@ -21,7 +21,7 @@ print(response.choices[0].message.content)
 with open("data/output.json", "r", encoding="utf-8") as f:
     rfp_data = json.load(f)
 
-for i, chunk in enumerate(rfp_data):
+for i, chunk in enumerate(rfp_data[:8]):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": f"RFP Data Chunk #{i}\n{chunk}"}],
@@ -40,22 +40,37 @@ with open("data/t_systems_data.json", "r", encoding="utf-8") as f:
     company_data = str(json.load(f))
 
 compare_instructions = """
+Next instructions:
 - Compare the memorized RFP data with the provided company's data.
-- For the RFP data, identify and note down the key fields and themes.
-- For the RFP data, evaluate the similarity between its key fields and the company's fields.
-- Assign a similarity score for each RFB cluster's field relative to the company's fields. The score should reflect the level of relevance and alignment between the RFP data and the company's operational areas.
+- Evaluate the similarity between RFP and the company's data in the following categories: Technology, Functional, Compliance, Domain.
+- Assign a similarity score for each category.
 - Scores can be in a simple numeric format (e.g., 1-10, where 10 is the highest similarity).
-- Present the results in a tabulated format.
-- For the company's fields, list its key fields alongside the corresponding similarity scores.
+- Present the results in the following format: Category_Name=Score.
 """
 
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "user", "content": company_data},
+    ],
+)
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
         {"role": "user", "content": compare_instructions},
     ],
 )
 
-print('Gotten response from compare instructions:')
+print('Gotten response from the scores evaluation:')
+print(response.choices[0].message.content)
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "Using all your obtained knowledge about RFP, make a summar with the key information, presented in a condensed form"},
+    ],
+)
+
+print('Gotten response from the summary forming:')
 print(response.choices[0].message.content)
