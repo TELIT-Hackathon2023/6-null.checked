d5ccb5bff83c416ee6ce79da2722b35ce86ebc40
Michael Ruzmetov <mimisha2014@gmail.com>
2023-11-26 10:40:24 +0100

    update prompt, generate new data

--- ai_backend/llm_deploy.py
+++ ai_backend/llm_deploy.py
@@ -8,9 +8,18 @@ system_instructions = """
     - Generate a brief version of the chunk's content without saying it is a chunk.
 """
 summary_prompt = """
-    Using all your obtained knowledge from the RFP data, 
-    make a summary with the key information on a project described in the RFP.
+    Utilizing the comprehensive insights gathered from the RFP data, 
+    create a detailed summary that encapsulates the essential elements of the project outlined in the RFP. 
+    Your summary should include:
+    1. The primary objectives and goals of the project.
+    2. Key technological requirements and specifications.
+    3. Critical functional needs and desired outcomes.
+    4. Any compliance and regulatory standards mentioned.
+    5. The specific industry/domain focus of the project.
+    6. Major deliverables and expected timelines.
+    Ensure that your summary is concise yet thorough, highlighting the most significant aspects of the project for a clear understanding.
 """
+
 brief_chunks = []
 
 
@@ -29,7 +38,7 @@ def send_rfp_data(client):
     with open("data/output.json", "r", encoding="utf-8") as f:
         rfp_data = json.load(f)
 
-    for i, chunk in enumerate(rfp_data[:5]):
+    for i, chunk in enumerate(rfp_data[:8]):
         brief_chunk = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "system", "content": system_instructions}]+\
@@ -52,7 +61,7 @@ def get_summary(client):
     
     return summary
 
-def get_scores(client):
+def get_scores(client, summary):
     with open("data/t_systems_data.json", "r", encoding="utf-8") as f:
         company_data = str(json.load(f))
 
@@ -61,7 +70,7 @@ def get_scores(client):
     - Evaluate the similarity between RFP and the company's data in the following categories: Technology, Functional, Compliance, Domain.
     - Assign a similarity score for each category.
     - Scores can be in a simple numeric format (e.g., 1-10, where 10 is the highest similarity).
-    - Present the results strictly in the following format: CATEGORY_NAME=SCORE.
+    - Present the results strictly in the following format: Technology=N.
     """
 
     scores = {}
@@ -70,7 +79,7 @@ def get_scores(client):
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": compare_instructions},
-                {"role": "user", "content": "RFP Data:\n"+'\n'.join([chunk["content"] for chunk in brief_chunks])},
+                {"role": "user", "content": f"RFP Data: {summary}"},
                 {"role": "user", "content": f"Company Data:\n{company_data}"}
             ],
         )
@@ -84,5 +93,5 @@ def run_llm():
     client = get_client()
     send_rfp_data(client)
     summary = get_summary(client)
-    scores = get_scores(client)
+    scores = get_scores(client, summary)
     return scores, summary
--- ai_backend/test_data_dict.py
+++ ai_backend/test_data_dict.py
@@ -1,4 +1,26 @@
 TEST_DATA_DICT = {
+    "https://github.com/TELIT-Hackathon2023/6-null.checked/blob/main/ai_backend/data/rfps/170609_student.pdf": {
+        "overall_score": 0.75,
+        "summary": "The primary objective of this project is to design, develop, and implement an Online Integrated Student Life Cycle Management System. The goal is to streamline and automate various processes related to student life cycle management, including admissions, enrollment, fee management, examination management, and certification. \n\nKey technological requirements for the system include a web-based platform that is scalable, secure, and user-friendly. It should have modules for different functions like student information management, course management, attendance tracking, result processing, and document management. The system should be able to integrate with existing systems and databases in educational institutions and ensure data security and privacy.\n\nThe critical functional needs and desired outcomes of the project include improving efficiency and transparency in student life cycle management processes, reducing manual paperwork, enhancing data accuracy, facilitating online fee payments, providing real-time access to information for students, teachers, and administrators, and generating reports and analytics for decision-making.\n\nThe project must comply with relevant regulatory and compliance standards set by the government, including data protection and privacy regulations. The system should adhere to security standards and protocols to ensure the confidentiality and integrity of student data.\n\nThe specific industry/domain focus of the project is the education sector, specifically higher education institutions and universities. The system aims to address the challenges and complexities associated with managing student life cycles in these institutions.\n\nThe major deliverables of the project include a fully functional Online Integrated Student Life Cycle Management System that meets the specified requirements. Additionally, a service level agreement (SLA) should be established for ongoing maintenance and support of the system. The expected timeline for the project will be specified in the tender documents, including milestones for development, testing, deployment, and training.\n\nOverall, the project aims to enhance the efficiency and effectiveness of student life cycle management processes in educational institutions through the implementation of a comprehensive online system.",
+        "matching_scores": [
+            {
+                "score": 1.0,
+                "section": "Technology"
+            },
+            {
+                "score": 1.0,
+                "section": "Functional"
+            },
+            {
+                "score": 1.0,
+                "section": "Compliance"
+            },
+            {
+                "score": 0.0,
+                "section": "Domain"
+            }
+        ]
+    },
     "https://github.com/TELIT-Hackathon2023/6-null.checked/blob/main/ai_backend/data/rfps/5308.pdf": {
         "overall_score": 3.0,
         "summary": "Based on the provided information from the RFP, here is a concise summary of the project:\n\n1. Primary objectives and goals:\n   - Selection of a Software Development Agency (SDA) for the development and maintenance of a Web Portal with VAT support modules.\n   - Migration of the existing application (COVIS) to the new portal.\n   - Maintenance and support of VAT-related functionalities, including return forms.\n\n2. Key technological requirements and specifications:\n   - Development and maintenance of a web portal with specific functionalities.\n   - COVIS application migration to the new portal.\n   - Database administration, software change management, and system documentation.\n   - Compliance with CMS Portal, e-Payment of Taxes, and VAT-related regulations.\n   - Emphasis on application architecture and solution design.\n\n3. Critical functional needs and desired outcomes:\n   - Successful migration of the COVIS application to the new portal.\n   - Development and maintenance of VAT-related functionalities, including return forms.\n   - Timely delivery of project milestones and adherence to the project plan.\n   - System study, solution development, testing, and user acceptance testing.\n\n4. Compliance and regulatory standards mentioned:\n   - Compliance with CMS Portal, e-Payment of Taxes, and VAT-related regulations.\n   - Adherence to the provided clause reference and compliance sheets.\n\n5. Industry/domain focus:\n   - The project is focused on the development and maintenance of a Web Portal with VAT support modules, catering to VAT-related processes.\n\n6. Major deliverables and expected timelines:\n   - Timely completion of system study, solution design, solution development, testing, user acceptance testing, and acceptance testing by the assessment agency.\n   - Go-Live of the new portal.\n   - Adherence to the provided project plan and documentation requirements.\n\nThis summary provides an overview of the main objectives, technological requirements, functional needs, compliance standards, industry focus, and deliverables of the project outlined in the RFP.",
@@ -42,5 +64,27 @@ TEST_DATA_DICT = {
                 "section": "Domain"
             }
         ]
-    }
+    },
+    "https://github.com/TELIT-Hackathon2023/6-null.checked/blob/main/ai_backend/data/rfps/DRA-0131_2015_08_04_09_40_16_KHV9j_Af8vT_59n8t.pdf": {
+        "overall_score": 3.5,
+        "summary": "In summary, the primary objectives and goals of the project are to engage a qualified contractor to provide market research support services for the Marketing and Technology Division of the City of Charlotte. The key technological requirements and specifications include coordination with CATS officials and adherence to safe practices and procedures. The critical functional needs are to maintain a safe environment in managed facilities and improve transit services in the Charlotte-Mecklenburg area. The desired outcomes are to understand customer satisfaction and address public transit issues. Compliance with safety policies, regulations, and statutory requirements is emphasized. The specific industry/domain focus of the project is public transportation and transit services. The major deliverables include conducting a market research study each year, measuring customer satisfaction, and improving service. The expected timeline is from the date of award through September, with renewal options.",
+        "matching_scores": [
+            {
+                "score": 3.0,
+                "section": "Technology"
+            },
+            {
+                "score": 1.0,
+                "section": "Functional"
+            },
+            {
+                "score": 1.0,
+                "section": "Compliance"
+            },
+            {
+                "score": 9.0,
+                "section": "Domain"
+            }
+        ]
+    },
 }
