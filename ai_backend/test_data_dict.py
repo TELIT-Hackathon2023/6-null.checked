d9b95d7368b85e739d51f1ae696d7a5106afb8e3
Michael Ruzmetov <mimisha2014@gmail.com>
2023-11-26 10:25:18 +0100

    add test data dict

---
+++ ai_backend/test_data_dict.py
@@ -0,0 +1,46 @@
+TEST_DATA_DICT = {
+    "https://github.com/TELIT-Hackathon2023/6-null.checked/blob/main/ai_backend/data/rfps/5308.pdf": {
+        "overall_score": 3.0,
+        "summary": "Based on the provided information from the RFP, here is a concise summary of the project:\n\n1. Primary objectives and goals:\n   - Selection of a Software Development Agency (SDA) for the development and maintenance of a Web Portal with VAT support modules.\n   - Migration of the existing application (COVIS) to the new portal.\n   - Maintenance and support of VAT-related functionalities, including return forms.\n\n2. Key technological requirements and specifications:\n   - Development and maintenance of a web portal with specific functionalities.\n   - COVIS application migration to the new portal.\n   - Database administration, software change management, and system documentation.\n   - Compliance with CMS Portal, e-Payment of Taxes, and VAT-related regulations.\n   - Emphasis on application architecture and solution design.\n\n3. Critical functional needs and desired outcomes:\n   - Successful migration of the COVIS application to the new portal.\n   - Development and maintenance of VAT-related functionalities, including return forms.\n   - Timely delivery of project milestones and adherence to the project plan.\n   - System study, solution development, testing, and user acceptance testing.\n\n4. Compliance and regulatory standards mentioned:\n   - Compliance with CMS Portal, e-Payment of Taxes, and VAT-related regulations.\n   - Adherence to the provided clause reference and compliance sheets.\n\n5. Industry/domain focus:\n   - The project is focused on the development and maintenance of a Web Portal with VAT support modules, catering to VAT-related processes.\n\n6. Major deliverables and expected timelines:\n   - Timely completion of system study, solution design, solution development, testing, user acceptance testing, and acceptance testing by the assessment agency.\n   - Go-Live of the new portal.\n   - Adherence to the provided project plan and documentation requirements.\n\nThis summary provides an overview of the main objectives, technological requirements, functional needs, compliance standards, industry focus, and deliverables of the project outlined in the RFP.",
+        "matching_scores": [
+            {
+                "score": 5.0,
+                "section": "Technology"
+            },
+            {
+                "score": 2.0,
+                "section": "Functional"
+            },
+            {
+                "score": 1.0,
+                "section": "Compliance"
+            },
+            {
+                "score": 4.0,
+                "section": "Domain"
+            }
+        ]
+    },
+    "https://github.com/TELIT-Hackathon2023/6-null.checked/blob/main/ai_backend/data/rfps/2015_RFPWebsiteRedesignRepost.pdf": {
+        "overall_score": 1.75,
+        "summary": "The State Bar of California is seeking proposals to redesign and redevelop their public-facing website, with a focus on accessibility, modernization, and optimization for mobile and other devices. The current website is outdated and does not meet the latest accessibility standards. The objective of the project is to create an accessible and aesthetically pleasing website with intuitive navigation. Key requirements include improving navigation, search capabilities, and mobile compatibility. The website serves three primary target audiences: Bar applicants, members, and the public. It provides various services and information for each constituency. The website is currently hosted at the State of California's Office of Technology Services and uses the DotNetNuke content management system. The programming language is C#.Net, and the databases used are MS SQL and IBM DB2. The website receives the majority of its traffic from the United States, with other traffic coming from Canada, the Philippines, the UK, and India. The majority of users access the website on desktop/laptop devices, followed by mobile and tablet devices. The most commonly used browsers are Chrome, IE, Safari, and Firefox. The website is currently only available in English, but the search feature supports all languages. The website needs to be ADA compliant based on WCAG guidelines. The project goals and objectives include improving accessibility, navigation, search engine optimization, and overall user experience. The deliverables of the project include a Proof-of-Concept design, analysis of navigation and site architecture, accessible design evaluation, search engine optimization recommendations, content strategy recommendations, visual design, front-end coding, site map, wireframes, HTML/CSS templates, style guide, email template, and user testing and analysis.",
+        "matching_scores": [
+            {
+                "score": 4.0,
+                "section": "Technology"
+            },
+            {
+                "score": 2.0,
+                "section": "Functional"
+            },
+            {
+                "score": 1.0,
+                "section": "Compliance"
+            },
+            {
+                "score": 0.0,
+                "section": "Domain"
+            }
+        ]
+    }
+}
